0d947907b6b3fff63e3b0f1ef94674ba4c93c557
Rachit Gaur <81481000+iamrachitgaur@users.noreply.github.com>
2023-10-26 18:18:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const port = process.env.PORT || 4200;
 const userRouter = require('./routes/userRouter')
 const productRouter = require('./routes/productRouter')
 
-app.use(express.static('../dist/eshopping'));
+app.use(express.static('./dist/eshopping'));
 app.use(express.json())
 app.use('/userApi',userRouter)
 app.use('/productApi',productRouter)
 app.get('/*', (req, res) =>
-    res.sendFile('index.html',{root:'../dist/eshopping/'})
+    res.sendFile('index.html',{root:'./dist/eshopping/'})
 );
 
 // Start the app by listening on the default Heroku port
